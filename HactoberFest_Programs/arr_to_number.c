35df91114859f49a9d43bc75b46786f03686be3a
Ajaykarthik224 <57026734+Ajaykarthik224@users.noreply.github.com>
2019-10-26 11:21:52 +0530

    Update arr_to_number.c

--- HactoberFest_Programs/arr_to_number.c
+++ HactoberFest_Programs/arr_to_number.c
@@ -2,14 +2,16 @@
 #include<stdlib.h>
 #include<math.h>
 int main(){
-	int i,dig,number=0;
+	int i,dig;
+	double number=0;
 	//Read a number to complete the program.
+	scanf("%d",&dig);
 	int arr[dig];
 	for(i=0;i<dig;i++){
 		printf("\nEnter %dth digit: ",i+1);
 		scanf("%d",&arr[i]);
 		number=number+arr[i]*pow(10,i);
 	}
-	printf("\nThe number is: %d\n",number);
+	printf("\nThe number is: %lf\n",number);
 return 0;
 }
